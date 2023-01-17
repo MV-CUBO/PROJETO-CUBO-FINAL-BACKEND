e2391362d9318270353916cf3d658da518980801
TicianoFilho <ticianofilho@gmail.com>
2023-01-17 15:37:58 -0300

    fix: change id generationType to AUTO

--- API-Health/src/main/java/br/com/mv/APIHealth/domain/entity/Person.java
+++ API-Health/src/main/java/br/com/mv/APIHealth/domain/entity/Person.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private String name;
     private String cpf;
