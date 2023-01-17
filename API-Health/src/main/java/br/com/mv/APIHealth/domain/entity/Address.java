20b269bb2e27ac1b2fa1b6a895d70ec7ab7a51d7
TicianoFilho <ticianofilho@gmail.com>
2023-01-17 16:08:58 -0300

    feat: add Address entity

---
+++ API-Health/src/main/java/br/com/mv/APIHealth/domain/entity/Address.java
@@ -0,0 +1,36 @@
+package br.com.mv.APIHealth.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String zipCode;
+
+    private String street;
+
+    private String number;
+
+    private String district;
+
+    private String city;
+
+    private String state;
+
+    private String complements;
+}
