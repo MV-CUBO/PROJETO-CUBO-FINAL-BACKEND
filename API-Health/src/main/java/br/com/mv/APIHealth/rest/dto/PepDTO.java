4fe19ecaeaa4113a4b4bb1d5d289c00e5e0f3760
Thiago Cavalcante <thiagocms13@gmail.com>
2023-01-19 16:32:36 -0300

    feat: add controller of pep

--- API-Health/src/main/java/br/com/mv/APIHealth/rest/dto/PepDTO.java
+++ API-Health/src/main/java/br/com/mv/APIHealth/rest/dto/PepDTO.java
@@ -4,26 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PepDTO {
+
+    @NotEmpty
     private String pepNumber;
-
+    @NotEmpty
     private UUID patientId;
-
+    @NotEmpty
     private UUID doctorId;
-
+    @NotEmpty
     private String status;
-
+    @NotEmpty
     private String prescription;
-
+    @NotEmpty
     private String boodType;
-
+    @NotEmpty
     private String allergies;
-
+    @NotEmpty
     private LocalDateTime createdAt;
 
     private LocalDateTime updateAt;
