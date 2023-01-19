1e7ad3f8f5ff2ae150030779b580e472aa192b96
Luiz Castro <luizjsp@yahoo.com.br>
2023-01-19 17:22:32 -0300

    FEAT: doctorcontroller doctorservice doctorservice impl

--- API-Health/src/main/java/br/com/mv/APIHealth/service/DoctorService.java
+++ API-Health/src/main/java/br/com/mv/APIHealth/service/DoctorService.java
@@ -1,4 +1,24 @@
 package br.com.mv.APIHealth.service;
 
+import br.com.mv.APIHealth.domain.entity.Doctor;
+import br.com.mv.APIHealth.rest.dto.DoctorDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface DoctorService {
+
+    public DoctorDTO create (DoctorDTO doctorDTO);
+
+    public DoctorDTO getDoctorById (UUID id);
+
+    public List<Doctor> getAll ();
+
+    public DoctorDTO update (UUID id, DoctorDTO doctorDTO );
+
+    public void delete (UUID id);
+
+
+
+
 }
